380c7d24b135fcfeec6cca98937e00942238cf3e
Mahdi Ghasemi <ghasemim875@gmail.com>
2024-01-25 10:27:59 +0330

    django basics

---
+++ learning_log/urls.py
@@ -0,0 +1,12 @@
+"""Defines URL patterns for learning_logs."""
+
+from django.urls import path
+from . import views
+
+app_name = 'learning_log'
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('topics', views.topics, name='topics'),
+    path('topics/<int:topic_id>', views.topic, name='topic')
+]
--- learning_log/views.py
+++ learning_log/views.py
@@ -1,3 +1,28 @@
 from django.shortcuts import render
+from .models import Topic
 
 # Create your views here.
+
+
+def index(request):
+    """The home page for Learning Log."""
+
+    return render(request, 'learning_log/index.html')
+
+
+def topics(request):
+    topics = Topic.objects.order_by('date_added')
+    context = {'topics': topics}
+
+    return render(request, 'learning_log/topics.html', context)
+
+
+def topic(request, topic_id):
+    topic = Topic.objects.get(id=topic_id)
+    entries = topic.entry_set.order_by('-date_added')
+    context = {
+        'topic': topic,
+        'entries': entries
+    }
+
+    return render(request, 'learning_log/topic.html', context)
